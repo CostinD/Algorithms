8fdb1b880fcad65b7a0c6727ec95ec64d8be3819
Costin Denisov <gralexd@gmail.com>
2015-01-26 11:20:59 -0500

    Added First Version of GUIAlgorithms
    First Version

---
+++ GraphicSortAlgorithmsCollection/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraphicSortAlgorithmsCollection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SortButton_Click(object sender, RoutedEventArgs e)
+        {
+            string[] input = DataTextBox.Text.Split(',');
+            
+            int[] n = new int[input.Length];
+            for (int i = 0; i < input.Length; i++ )
+            {
+                n[i] = int.Parse(input[i]);
+            }
+            SortingAlgorithms.InsertionSort(ref n);
+   
+            DataTextBox.Text = "";
+
+            for(int i = 0; i < n.Length; i++)
+            {
+                DataTextBox.Text += n[i];
+                if (i != n.Length - 1)
+                    DataTextBox.Text += ",";
+            }
+        }
+    }
+}
---
+++ GraphicSortAlgorithmsCollection/SortingAlgorithms.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicSortAlgorithmsCollection
+{
+    class SortingAlgorithms
+    {
+        public static void InsertionSort(ref int[] input)
+        {
+            for(int i = 0; i < input.Length; i++)
+            {
+                int x = input[i];
+                for (int j = i; j > 0; j--)
+                {
+                    if (input[j] < input[j - 1])
+                        Swap(ref input[j], ref input[j - 1]);
+                }
+            }
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
